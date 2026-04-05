bb5161281d48d0acde34f8cfb6628d219ecbb57a
OgoAmiel <Ogomadisa@gmail.com>
2026-04-05 15:57:35 +0200

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,7 @@
 <script setup>
-import { onMounted, ref } from 'vue'
-
-const message = ref('Loading backend...')
-
-onMounted(async () => {
-  try {
-    const response = await fetch('http://127.0.0.1:8000/api/health/')
-    const data = await response.json()
-    message.value = data.message
-  } catch (error) {
-    message.value = 'Could not connect to backend'
-    console.error(error)
-  }
-})
+import Working from './views/Working.vue'
 </script>
 
 <template>
-  <main>
-    <h1>Ogorogile Frontend</h1>
-    <p>{{ message }}</p>
-  </main>
+  <Working />
 </template>
-
-<style scoped>
-main {
-  padding: 2rem;
-}
-</style>
---
+++ src/views/Working.vue
@@ -0,0 +1,3 @@
+<template src="./working.html"></template>
+<script src="./working.js"></script>
+<style scoped src="./working.css"></style>
